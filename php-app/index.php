775612edb87e6c3153afbfcfad84b9e03be21de3
Burenzaya <bvrnee1009@gmail.com>
2023-12-09 09:15:03 +0800

    image form

--- php-app/index.php
+++ php-app/index.php
@@ -30,9 +30,9 @@ $mysqli->query("CREATE TABLE IF NOT EXISTS orders (
 )");
 
 $mysqli->query("CREATE TABLE IF NOT EXISTS user_img (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    img_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    img_data VARBINARY(MAX),
+    img_data LONGBLOB,
     FOREIGN KEY (user_id) REFERENCES users(id)
 )");
 
@@ -44,7 +44,7 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = $_POST['email'];
 
         $userInsertQuery = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
-        
+
         if ($mysqli->query($userInsertQuery) === TRUE) {
             echo "<br>Хэрэглэгчийн мэдээллийг амжилттай нэмлээ";
         } else {
@@ -57,48 +57,74 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         $productName = $_POST['product_name'];
 
         $orderInsertQuery = "INSERT INTO orders (user_id, product_name) VALUES ('$userId', '$productName')";
-        
+
         if ($mysqli->query($orderInsertQuery) === TRUE) {
             echo "<br>Захиалгын мэдээллийг амжилттай нэмлээ";
         } else {
             echo "Алдаа: " . $orderInsertQuery . "<br>" . $mysqli->error;
         }
     }
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['clear_data'])) {
-    
-        $deleteUsersQuery = "DELETE FROM orders";
-        if ($mysqli->query($deleteUsersQuery) === TRUE) {
-            echo "<br>Захиалгын мэдээллийг амжилттай устгалаа";
-        } else {
-            echo "Алдаа " . $mysqli->error;
-        }
 
-        $deleteUsersQuery = "DELETE FROM users";
-        if ($mysqli->query($deleteUsersQuery) === TRUE) {
-            echo "<br>Хэрэглэгчийн мэдээллийг амжилттай устгалаа";
+    if (isset($_POST['submit_img'])) {
+        if (isset($_FILES['imageUpload']) && $_FILES['imageUpload']['error'] === UPLOAD_ERR_OK) {
+
+            $user_id = $_POST['user_id'];
+            $imgTmpName = $_FILES['imageUpload']['tmp_name'];
+            $imgData = file_get_contents($imgTmpName);
+
+            $stmt = $mysqli->prepare("INSERT INTO user_img (user_id, img_data) VALUES (?, ?)");
+            $stmt->bind_param("is", $user_id, $imgData);
+            $stmt->execute();
+
+            if ($stmt->affected_rows > 0) {
+                echo "Зураг амжилттай нэмэгдлээ";
+            } else {
+                echo "Алдаа_1" . $mysqli->error;
+            }
         } else {
-            echo "Алдаа " . $mysqli->error;
+            echo "Алдаа_2";
         }
-    
-        // You can perform similar delete operations for other tables or data as needed
     }
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['show_data'])) {
-    
+
+    if (isset($_POST['clear_data'])) {
+
+        if (($mysqli->query($resetAutoIncrementQuery = "ALTER TABLE orders AUTO_INCREMENT = 1") &&
+            $mysqli->query($deleteUsersQuery = "DELETE FROM orders") &&
+
+            $mysqli->query($resetAutoIncrementQuery = "ALTER TABLE user_img AUTO_INCREMENT = 1") &&
+            $mysqli->query($deleteUsersQuery = "DELETE FROM user_img") &&
+
+            $mysqli->query($resetAutoIncrementQuery = "ALTER TABLE users AUTO_INCREMENT = 1") &&
+            $mysqli->query($deleteUsersQuery = "DELETE FROM users"))  === 1) {
+            echo "Done";
+        }
+    }
+
+    if (isset($_POST['show_data'])) {
+
         echo "<h1>Users</h1>";
         $resultUsers = $mysqli->query("SELECT * FROM users");
         while ($row = $resultUsers->fetch_assoc()) {
             echo "<p>Хэрэглэгчийн ID: {$row['id']}, Нэр: {$row['name']}, И-майл: {$row['email']}</p>";
         }
-        
+
         echo "<h1>Orders</h1>";
         $resultOrders = $mysqli->query("SELECT * FROM orders");
         while ($row = $resultOrders->fetch_assoc()) {
             echo "<p>Захиалгын ID: {$row['id']}, Хэрэглэгчийн ID: {$row['user_id']}, Барааны нэр: {$row['product_name']}</p>";
         }
-    }
+        
+        $result = $mysqli->query("SELECT img_id, user_id FROM user_img");
 
+        $result = $mysqli->query("SELECT img_data FROM user_img");
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '<img src="data:image/jpeg;base64,' . base64_encode($row['img_data']) . '" />';
+            }
+        } else {
+            echo 'No images found';
+        }
+    }
 }
 $mysqli->close();
-?>
